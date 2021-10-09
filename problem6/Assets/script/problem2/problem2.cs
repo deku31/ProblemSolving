7456912f21ebd39a152b76a0b15cdf7dded48422
Deri Kurniawan <akuntoramdeku00@gmail.com>
2021-10-09 18:59:17 +0700

    problem6

---
+++ problem6/Assets/script/problem2/problem2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class problem2 : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    //besar gaya dorongan awal pada bola
+    public float xInitialForce;
+    public float yInitialForce;
+    public float speed;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        //mengubah posisi dingah ketika awal permainan
+        transform.localPosition=new Vector3(0,0,0);
+    }
+    void gerak()
+    {
+        // Gunakan gaya untuk menggerakkan linkaran.
+        rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce) * speed);
+        if (rigidBody2D.velocity.magnitude != speed)
+        {
+            rigidBody2D.velocity = rigidBody2D.velocity.normalized * speed;
+        }
+    }
+    void Update()
+    {
+        Invoke("gerak", 0);
+    }
+}
---
+++ problem6/Assets/script/problem7/ballfollowP7.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballfollowP7 : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 dir;
+    public float speed;
+    public Transform point;
+
+    //kondisi dimana jika true maka objek bergerak menuju mouse
+    public bool run;
+    void Start()
+    {
+        //mengamb komponene rigibody2d
+        rb = GetComponent<Rigidbody2D>();
+        //mengatur posisi objek ditengah
+        transform.position = new Vector3(0, 0, 0);
+        point.position = transform.position;
+    }
+    private void Update()
+    {
+        //mengubah posisi point ketempat posisi mouse di klik 
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            //membaca posisi mouse
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            point.position = mousePosition;
+            run = true;
+        }
+        if (run==true)
+        {
+            dir = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector2(dir.x * speed, dir.y * speed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+        
+        
+    }
+}
