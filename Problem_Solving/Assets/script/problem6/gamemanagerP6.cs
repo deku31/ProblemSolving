e6f12c1876eb6e4938f6fe0024a4cde8ea1e4aaf
Deri Kurniawan <akuntoramdeku00@gmail.com>
2021-10-10 11:09:56 +0700

    problem 10

---
+++ Problem_Solving/Assets/script/problem6/gamemanagerP6.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanagerP6 : MonoBehaviour
+{
+    //prefab dari cube
+    public GameObject cube;
+    //jumlah cube yang keluar
+    private int cubelength;
+    //jumlah maksimal cube
+    public int jumlahCube;
+    //untuk menghitung cube yang keluar
+    private GameObject[] getCount;
+
+    void Start()
+    {
+        //mengacak jumlah maksimal cube
+        jumlahCube = Random.Range(1, 10);
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        getCount = GameObject.FindGameObjectsWithTag("cube");
+        cubelength = getCount.Length;
+        //memunculkan cube
+        if (cubelength<jumlahCube)
+        {
+            Instantiate(cube);
+        }
+    }
+}
---
+++ Problem_Solving/Assets/script/problem8/gameManagerP8.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManagerP8 : MonoBehaviour
+{
+    //mencegah munculnya cube di posisi ball
+    public Transform ball;
+
+    //mengatur agar cube muncul setiap 3 detik
+    public float time = 3f;
+    public bool p;
+
+    //mengatur cube
+    public GameObject cube;
+    private int cubelength;
+    public int jumlahCube;
+    private GameObject[] getCount;
+
+    //posisi random cube
+    public float horizontal=0;
+    public float vertical;
+    private float ranx, rany;
+
+    //kondisi dimana cube bertambah atau tidak
+    public bool tambah;
+    //score
+    public int score;
+    public Text scoreTxt;
+
+
+    void Start()
+    {
+        tambah = true;
+        jumlahCube = Random.Range(4, 10);
+       
+    }
+    // Start is called before the first frame update
+   // Update is called once per frame
+    void Update()
+    {
+        ranx = Random.Range(-horizontal, horizontal);
+        rany = Random.Range(-vertical, vertical);
+        if (Input.GetKey(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+       
+        getCount = GameObject.FindGameObjectsWithTag("cube");
+        cubelength = getCount.Length;
+        if (cubelength < jumlahCube && tambah == true)
+        {
+            
+            if(ranx>2+transform.position.x||ranx<-2+transform.position.x)
+            {
+                Instantiate(cube, new Vector2(ranx, rany), Quaternion.identity);
+            }
+        }
+        else if (cubelength == jumlahCube)
+        {
+            tambah = false;
+            time = 3f;
+
+        }
+
+        if (time > 0 && p == false)
+        {
+            time -= 0.05f;
+        }
+        else if(time<=0)
+        {
+            tambah = true;
+        }
+        scoreTxt.text = score.ToString();
+        IEnumerator ToSpawnTrue()
+        {
+            yield return new WaitForSeconds(0.75f);
+            tambah = true;
+        }
+    }
+    
+}
